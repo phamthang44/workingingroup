861a072d40558cc0c22cbd44c4653f0eb023864d
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2024-08-08 09:22:47 +0700

    js update

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const navbar = document.querySelector('.nav')
+const ulElement = navbar.querySelector('ul')
+const liList = ulElement.querySelectorAll('li')
+
+function eventHandler (list) {
+    for (let i = 0; i < list.length; i++) {
+
+        let liItem = list[i]
+
+        liItem.onclick = function() {
+
+            list.forEach(li => {
+                li.classList.remove('active')
+            });
+
+            this.classList.add('active'), 1000
+
+        }
+    }
+}
+
+eventHandler(Array.from(liList))
+
+// console.log(123
+//.nav li.active a::after
